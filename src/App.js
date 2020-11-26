c6332aadcf209cc695ea92759dcea39ceb6856c2
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-11-26 22:59:27 +0100

    Changed App.js into a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShopContext from './context/shop-context'
 import ProductsPage from './pages/Products';
 import CartPage from './pages/Cart';
 import './App.css'
 
-class App extends Component {
-  state = {
-    products: [
-      { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
-      { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
-      { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
-      { id: 'p4', title: 'Half-dried plant', price: 2.99 }
-    ],
-    cart: [],
-    cartSum: 0,
-    addItem: () => {},
-    removeItem: () => {},
-  }
+const productList = [
+  { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
+  { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
+  { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
+  { id: 'p4', title: 'Half-dried plant', price: 2.99 }
+];
 
-   addProductToCart = (product) => {
-     console.log(product)
-    let updateCartSum = this.state.cartSum
-    const updatedCart = this.state.cart;
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [cartSum, setCartSum] = useState(0);
+
+  useEffect(() => {
+    setProducts(productList)
+  }, [])
+
+   const addProductToCart = (product) => {
+    let updateCartSum = cartSum
+    const updatedCart = cart;
 
     const updatedItemIndex = updatedCart.findIndex(// check if product exist in cart
       item => item.id === product.id
@@ -38,47 +39,46 @@ class App extends Component {
       updatedItem.quantity++;
       updatedCart[updatedItemIndex] = updatedItem;
     }
-      this.setState({cart: updatedCart, cartSum: updateCartSum})
+      setCart(updatedCart)
+      setCartSum(updateCartSum)
 }
 
- removeProductFromCart = (productId) => {
-  const updatedCart = this.state.cart;
-  const updatedItemIndex = updatedCart.findIndex(
-      item => item.id === productId
-    );
-
-    const updatedItem = {
-      ...updatedCart[updatedItemIndex]
-    };
-    updatedItem.quantity--;
-    if (updatedItem.quantity <= 0) {
-      updatedCart.splice(updatedItemIndex, 1);
-    } else {
-      updatedCart[updatedItemIndex] = updatedItem;
-    }
-    this.setState({cart: updatedCart})
-}
-
-  render() {
-    return (
-      <ShopContext.Provider value={
-        {
-         products: this.state.products,
-         cart: this.state.cart,
-         cartSum: this.state.cartSum,
-         addItem: this.addProductToCart,
-         removeItem: this.removeProductFromCart
-       }
-      }>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/" component={ProductsPage} exact />
-              <Route path="/cart" component={CartPage} exact />
-            </Switch>
-          </BrowserRouter>
-      </ShopContext.Provider>
-    );
+const removeProductFromCart = (productId) => {
+    const updatedCart = cart;
+    const updatedItemIndex = updatedCart.findIndex(
+        item => item.id === productId
+      );
+  
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex]
+      };
+      updatedItem.quantity--;
+      if (updatedItem.quantity <= 0) {
+        updatedCart.splice(updatedItemIndex, 1);
+      } else {
+        updatedCart[updatedItemIndex] = updatedItem;
+      }
+      setCart(updatedCart)
   }
+
+  return (
+    <ShopContext.Provider value={
+              {
+               products,
+               cart,
+               cartSum,
+               addItem: addProductToCart,
+               removeItem: removeProductFromCart
+             }
+            }>
+                <BrowserRouter>
+                  <Switch>
+                    <Route path="/" component={ProductsPage} exact />
+                    <Route path="/cart" component={CartPage} exact />
+                  </Switch>
+                </BrowserRouter>
+            </ShopContext.Provider>
+  );
 }
 
 export default App;
